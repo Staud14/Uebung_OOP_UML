a2459461dac594e29151d583ac97ad75756cf2af
Staudi <Staud14@users.noreply.github.com>
2018-12-17 16:13:43 +0100

    Added connection from "Fach" to "Lehrer" and "Klasse"

--- src/Fach.java
+++ src/Fach.java
@@ -1,3 +1,4 @@
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -5,10 +6,8 @@ public class Fach {
     private String name;
     private int wochenstunden;
     private Raumtyp raumanforderung;
-    private Set<Klasse> klasse = new TreeSet<Klasse>();
-
-    //TODO: Connection to Lehrer
-    //InProgress: Connection to Klasse
+    private Set<Klasse> klasse = new LinkedHashSet<Klasse>();
+    private Set<Lehrer> lehrer = new LinkedHashSet<Lehrer>();
 
     public String getName() {
         return name;
@@ -18,7 +17,23 @@ public class Fach {
         return wochenstunden;
     }
 
-    public Raumtyp getRaumanforderung(){
+    public void addKlasse(Klasse adding) {
+        klasse.add(adding);
+    }
+
+    public void deleteKlasse(Klasse removing) {
+        klasse.remove(removing);
+    }
+
+    public Raumtyp getRaumanforderung() {
         return raumanforderung;
+    }
+
+    public void addLehrer(Lehrer adding){
+        lehrer.add(adding);
+    }
+
+    public void deleteLehrer(Lehrer removing){
+        lehrer.remove(removing);
     }
 }
